da6b8158d841166247cfdc1937a9d12424cfe3a5
Squall <Squall>
2014-10-03 22:59:50 +0500

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SquallCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SquallCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
